b006db737b531bcfd603e5a396f28d3f2b773b61
suhyeon lee <tngus0673@naver.com>
2024-10-09 20:57:23 +0900

    KeychainAccess - modify  구현

--- Keychain_Example/Controllers/KeychainAccess/KA_ModifyViewController.swift
+++ Keychain_Example/Controllers/KeychainAccess/KA_ModifyViewController.swift
@@ -6,24 +6,47 @@
 //
 
 import UIKit
+import KeychainAccess
 
 class KA_ModifyViewController: UIViewController {
+
+    let modifyView = ModifyView()
+    private let keychain = Keychain(service: "login")
+    
+    override func viewWillAppear(_ animated: Bool) {
+        initTextField()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view = modifyView
+        
+        modifyView.btnModify.addTarget(self, action: #selector(btnModifyDidTap), for: .touchUpInside)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func btnModifyDidTap() {
+        if let id = modifyView.txtId.text, let newPwd = modifyView.txtNewPwd.text {
+            
+            do {
+                try keychain.set(newPwd, key: id)
+                print("ModifyViewController - update 성공")
+                print("id : \(id), newPassword : \(newPwd)")
+                modifyView.lblModify.text = "변경 성공! - id : \(id), newPassword : \(newPwd)"
+            }
+            catch let error {
+                print("error : \(error)")
+                print("ModifyViewController - update 실패")
+                modifyView.lblModify.text = "비밀번호 변경 실패"
+            }
+        }
     }
-    */
+    
+    // 텍스트 필드 초기화
+    private func initTextField() {
+        modifyView.txtId.text = ""
+        modifyView.txtNewPwd.text = ""
+        modifyView.lblModify.text = "비밀번호 변경 전 입니다."
+    }
 
 }
